3f7e8f72c931b511d4759c7cf572f3c36d2d457d
SleipnirMk1 <ilyasalefi@gmail.com>
2021-11-24 08:49:48 +0700

    handshake in udp, masih blom pake segment; args parse; listen

--- client.py
+++ client.py
@@ -1,17 +1,57 @@
 import socket
+import sys
 
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 8000)
+serverAddressPort   = "127.0.0.1"
 bufferSize          = 1024
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
+# Template sequence number, ganti jadi pake segment
+SEQ = 1
+
+# Argument Parser
+args_list = (str(sys.argv))
+port = int(sys.argv[1])
+path = args_list[2]
+
+def ThreeWayHandshakeClient():
+    # Send SYN
+    print("Three Way Handshake client start")
+    print("Sending SYN")
+    msg = str(SEQ)
+    bytesToSend = str.encode(msg)
+    UDPClientSocket.sendto(bytesToSend, (serverAddressPort, port))
+
+    # Receive SYN-ACK
+    SYNACK = UDPClientSocket.recvfrom(bufferSize)
+    print("Receiving SYN-ACK")
+
+    # Decode SYN-ACK
+    receive = SYNACK[0]
+    receive_arr = [int(i) for i in receive.split() if i.isdigit()]
+
+    SYN = receive_arr[0]
+    ACK = receive_arr[1]
+    print("SYN: {}, ACK: {}".format(SYN, ACK))
+
+    # Send ACK
+    print("Sending ACK")
+    ACK = SYN + 1
+    msg = str(ACK)
+    bytesToSend = str.encode(msg)
+    UDPClientSocket.sendto(bytesToSend, (serverAddressPort, port))
+
+    # Done
+    print("Three Way Handshake client done")
+
+# Testing
 # Send to server using created UDP socket
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+UDPClientSocket.sendto(bytesToSend, (serverAddressPort, port))
 
-msg = "Message from Server {}".format(msgFromServer[0])
-
-print(msg)
+# Handshake
+print("Waiting to start handshake")
+start_handshake, addr = UDPClientSocket.recvfrom(bufferSize)
+ThreeWayHandshakeClient()
--- server.py
+++ server.py
@@ -20,28 +20,84 @@ class Server:
     # allow broadcast
     self.server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     # 
-    self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    #self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     # bind server
+    print("Server start at port {}".format(port))
     self.server.bind(('localhost', port))
-    self.server.setblocking(0)
-
+    self.server.setblocking(1)
+    # parsed arguments
     self.port = port
     self.file_path = file_path
+    # list of clients
+    self.connections = []
+    # recv buffer size
+    self.buffer_size = 2**16
   
   def listen(self):
+    print("Listening to broadcast address for clients")
     listening_for_clients = True
     while listening_for_clients:
-      connections = []
-      msg, addr = self.server.recvfrom(2**16)
+      msg, addr = self.server.recvfrom(self.buffer_size)
+      print("[!] Connection from {} estabished".format(addr))
+      # add to list of clients
+      self.connections.append(addr)
+      # check if more clients
+      listen_more = input("[?] Listen more? (y/n): ")
+      if listen_more == "n":
+        listening_for_clients = False
+
+    # print list of clients found
+    print("{} Clients found:".format(len(self.connections)))
+    for i in range(len(self.connections)):
+      print("{}. {}".format(i+1, self.connections[i]))
+
+  def three_way_handshake(self, address):
+    # send message to start handshake
+      message = str.encode("Start handshake")
+      self.server.sendto(message, address)
+      print("Three Way Handshake server start")
+
+      # Template sequence number, ganti jadi pake segment
+      SEQ = 5
+
+      # Receive SYN
+      SYN, address = self.server.recvfrom(self.buffer_size)
+      print("Receiving SYN from {}".format(address))
+
+      # Decode SYN template, ganti jadi pake segment
+      #receive = SYN.decode('utf-8')
+      receive = SYN
+      receive_arr = [int(i) for i in receive.split() if i.isdigit()]
+      SYN = receive_arr[0]
+      print("SYN received: ", SYN)
+
+      # Send SYN-ACK
+      print("Sending SYN-ACK to {}".format(address))
+      ACK = SYN + 1
+      message = str(SEQ) + " " + str(ACK)
+      message = str.encode(message)
+      self.server.sendto(message, address)
+
+      # Receive ACK
+      ACK, address = self.server.recvfrom(self.buffer_size)
+      print("Receiving ACK from {}".format(address))
+
+      # Decode ACK template, ganti jadi pake segment
+      #receive = ACK.decode('utf-8')
+      receive = ACK
+      receive_arr = [int(i) for i in receive.split() if i.isdigit()]
+      ACK = receive_arr[0]
+      print("ACK received: ", ACK)
+        
+      # Done
+      print("Three Way Handshake server done with {}".format(address))
 
   def send_file(self):
-    # three way handshake
+    # handshake to all clients in list
+    for client in self.connections:
+      self.three_way_handshake(client)
     # ambil file dan segmentasi
     # kirim pake algoritma Go-Back-N
-    pass
-
-  def three_way_handshake(self):
-    pass
 
 
 
@@ -68,7 +124,10 @@ class Server:
 #   server_socket.sendto(bytesToSend, address)
 
 if __name__ == "__main__":
+  # parse arguments
   args_list = (str(sys.argv))
-  server_socket = Server()
+  server_socket = Server(int(sys.argv[1]), args_list[2])
+  # listen
   server_socket.listen()
+  # send
   server_socket.send_file()
